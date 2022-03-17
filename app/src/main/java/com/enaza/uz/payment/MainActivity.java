79ede5d1f8d0646e4cc49de7d8226e94dfe48e28
lewan42 <lewanov888@gmail.com>
2022-03-17 19:57:42 +0500

    add logs

--- app/src/main/java/com/enaza/uz/payment/MainActivity.java
+++ app/src/main/java/com/enaza/uz/payment/MainActivity.java
@@ -1,8 +1,15 @@
 package com.enaza.uz.payment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import com.enaza.uz.R;
+import com.enaza.uz.payment.utils.PaycomSandBox;
+
+import static com.enaza.uz.payment.PaymentActivity.EXTRA_AMOUNT;
+import static com.enaza.uz.payment.PaymentActivity.EXTRA_ID;
+import static com.enaza.uz.payment.PaymentActivity.EXTRA_LANG;
+import static com.enaza.uz.payment.PaymentActivity.EXTRA_SAVE;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+
+         String xAuth = "6229d432f193c361ee0718bb";
+          Double sum = 5551.0;
+         Boolean multyPaying = false;
+
+        Intent intent = new Intent(MainActivity.this, PaymentActivity.class);
+        intent.putExtra(EXTRA_ID, xAuth); //Ваш ID мерчанта
+
+        intent.putExtra(EXTRA_AMOUNT, sum); //Сумма оплаты
+        intent.putExtra(EXTRA_SAVE, multyPaying); //Сохранить для многократной оплаты?
+        intent.putExtra(EXTRA_LANG, "RU"); //Язык "RU" или "UZ"
+        PaycomSandBox.setEnabled(true); //true для тестовой площадки, по умолчанию false
+        startActivityForResult(intent, 0);
     }
 }
