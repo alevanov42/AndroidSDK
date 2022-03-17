ed88019e0f8dd4c4a003b212591fa3cbd289f3ef
alevanov <alevanov@enazamusic.com>
2022-03-17 18:06:02 +0500

    new version

---
+++ app/src/main/java/com/enaza/uz/payment/MainActivity.java
@@ -0,0 +1,14 @@
+package com.enaza.uz.payment;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.enaza.uz.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
